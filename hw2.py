b2e9692e1e60edf26eda6c41ea8bf8f7a0e1f3ce
Ibrahim Abdirizak Abdi <abdiibra@dh2010pc24.utm.utoronto.ca>
2020-01-22 16:46:38 -0500

    finnished coding need to do witten + code clean up

--- hw2.py
+++ hw2.py
@@ -12,7 +12,10 @@ U = +3
 example_float = ([1, 0, 0, 1, 0], 1, 1)
 example_float2 = ([2, 2, 2, 2, 2], 1, 1)
 
+
+total_num_floats = 2*(BASE-1)*math.pow(BASE, PRECISION-1)*(U-L+1)
 def is_valid_mantissa(int_lst,float_value):
+
     if int_lst[0] == 0 and float_value == 0:
         for x in int_lst[1:]:
             if x >= base or x<1:
@@ -44,6 +47,8 @@ def to_num(float_value):
         sum_mantissa += mantissa[i] / math.pow(3,i)
     
     return sum_mantissa * math.pow(3,exponent)*sign
+
+print(math.pow(BASE,L))
 
 
 def add_float(float1, float2):
@@ -77,32 +82,38 @@ def add_float(float1, float2):
         return (mantissa1, max_expo, 1)
 
     #mantissa1 is the mantissa with the higher exponent
+    #print(mantissa1)
+    #print(mantissa2)
     carry = 0
     i = 0 
     lim = 5 - shift 
     res = []
-    print(shift)
+    #print(shift)
     while(i < PRECISION):
         store = 0
+        #print('carry is ' + str(carry))
         if i < lim:
-            
-            sums = mantissa1[PRECISION - 1 - i] + mantissa2[PRECISION-1-i] + carry
+          #  print(str(mantissa1[PRECISION - 1 - i]) + "+" + str(mantissa2[PRECISION-1-i - shift]))
+            sums = mantissa1[PRECISION - 1 - i] + mantissa2[PRECISION-1-i - shift] + carry
             carry = sums // BASE
             store = sums%BASE
         else:
-            sums = mantissa1[i1] + carry
+         #   print('else ' + str(mantissa1[PRECISION - 1 - i])) 
+            sums = mantissa1[PRECISION - 1 - i] + carry
             carry = sums // BASE
             store = sums%BASE
         res = [store] + res
-        print(res)
+        #print(res)
         i+=1
 
-    if carry > 0:
+    if carry > 0 and max_expo == 3:
+        raise ValueError()  
+    elif carry > 0:
         res = [carry] + res
+        res.pop()
+    
+
     print(res)
-    i+=1    
-
-
     # Add your code here
     return (res[:5], max_expo, 1)
 
@@ -135,3 +146,11 @@ def find_nonzero_z(n):
 
 nonzero_zn = find_nonzero_z(1024)
 
+a = ([1,0,0,0,1], 2, 1) 
+b = ([2,0,0,0,0], 2, 1)
+c = ([0,0,0,0,1], 2, 1) 
+
+x = add_float(a,b)
+y = add_float(b,c)
+
+print(add_float(x,c) == add_float(a,y))
---
+++ tut/tut3.py
@@ -0,0 +1,10 @@
+import matplotlib
+
+import numpy as np
+import matplotlib.pyplot as plt
+%matplotlib inline
+
+
+from sklearn.datasets import load_iris
+iris = load_iris()
+print(iris['DESCR'])
